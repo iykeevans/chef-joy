3f0f83880c502f5bc5b884b829a43ee277aec314
Mistura <quadri.mistura@gmail.com>
2021-08-12 17:21:26 +0100

    reviews page

---
+++ components/reviews/top.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Stargazer from "../stargazer";
+import { ReviewData } from "./review data";
+
+function Top ({}) {
+    const [Reviewdata] = ReviewData
+    return <div>
+        <div className=" w-100 h-100 block pb-4 " >
+
+<span className="text-black">
+  <small>Home <span>/</span> </small>
+  <small>94601  <span>/</span> </small>
+  <small>Party  <span>/</span> </small>
+  <small className="text-gray-400">{Reviewdata.name}</small>
+</span>
+
+<div className="flex mt-12 w-100 justify-between">
+  <div className="flex">
+  <div className=" rounded-full w-20 bg-red-500 h-20">
+     
+  </div>
+  <div className="inline-block ml-4 pt-1.5">
+    <h1 className="font-bold text-xl">{Reviewdata.name}</h1>
+      <div className="flex h-8 mt-2 space-x-3">
+          <small className="rounded-full h-8 w-8 border bg-gray-300"></small>
+          <a className="text-xs mt-2">{Reviewdata.area}</a>
+          <a className="text-red-600 text-xs mt-2 underline">{`${Reviewdata.reviewNo} Reviews`}</a>
+          <div className="flex mt- justify-between">
+            <Stargazer stars={45}    />
+          </div>
+      </div>
+  </div>
+  </div>
+  <div className="">
+  <button
+      className="border border-black py-3 px-7 mt-6 font-medium md:text-base text-sm"
+      style={{ borderRadius: 8 }}
+    >
+      Back to profile
+    </button>
+  <button
+      className="bg-black text-white py-3 px-7 mt-6 ml-5 font-medium md:text-base text-sm"
+      style={{ borderRadius: 8 }}
+    >
+      Add review
+    </button>
+  </div>
+</div>
+
+</div>
+    </div>
+};
+
+export default Top
